d3cbd6bc8c5e87f7a71ba95c81ccccaa585e832b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-12-09 18:04:33 +0100

    feat: static files

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+set -e
+
+source project.config.sh
+
+"${DIR_PROJECT}/build.sh"
+
+"${DIR_APPS}/deploy.sh"
+
+firebase deploy --only hosting,firestore
