67635b7a3de3c124fce12e63cafafa05b8ade6a0
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-01-06 00:58:24 +0100

    feat: epifania quiz deployment

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ source project.config.sh
 "./runs/sanktuaria/deploy.sh" &
 "./runs/cuda-eucharystyczne/deploy.sh" &
 "./runs/ile-wiesz-o-bozym-narodzeniu/deploy.sh" &
+"./runs/ile-wiesz-o-epifanii/deploy.sh" &
 wait
 
 firebase deploy --only hosting,firestore
---
+++ runs/ile-wiesz-o-epifanii/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}"
+set -e
+
+source ../../project.config.sh
+
+CONTAINER_NAME="ile-wiesz-o-epifanii-20210106"
+CLOUDRUN_TAG="gcr.io/${GCP_PROJECT_ID}/${CONTAINER_NAME}"
+
+gcloud builds submit --tag "${CLOUDRUN_TAG}"
+
+gcloud beta run deploy "${CONTAINER_NAME}" \
+          --region "${GCP_REGION_CLOUDRUN}" \
+          --image "${CLOUDRUN_TAG}" \
+          --cpu 1 \
+          --memory 256Mi \
+          --max-instances 1 \
+          --timeout 5s \
+          --concurrency 15 \
+          --allow-unauthenticated \
+          --platform managed
+
